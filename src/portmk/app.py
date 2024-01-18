a15f5057d76f2601affeea9fd0075e0e006d3bc0
wesam <software.market999@gmail.com>
2024-01-18 13:56:22 +0400

    :fire: Remove unused

--- src/portmk/app.py
+++ src/portmk/app.py
@@ -171,19 +171,6 @@ pyglet.font.add_file(os.getcwd()+"\\fonts\\Poppins-Bold.ttf")
 font1 = ttk.font.Font(family="Poppins", size=12, weight="bold")
 font2= ttk.font.Font(family="Poppins", size=12, weight="normal")
 
-#runtimes
-runtimes=  [
-    "GODOT 4.1.3",
-    "GODOT 4.0.4",
-    "GODOT 4.1.3",
-    "GODOT 3.5.2",
-    "GODOT 3.4.5",
-    "GODOT 3.3.4",
-    "GODOT 3.2.3",
-    "GODOT 3.0.6",
-    "GODOT 2.1.6",
-
-]
 
 #setup styles
 style1 = ttk.Style()
@@ -218,11 +205,6 @@ browse_file_gptk_Entry = ttk.Entry(window, font=(font1, 16), width=30)
 browse_file_gptk_Entry.pack(pady=3)
 browse_file_gptk_btn = ttk.Button(window, text="Browse",style="success.Outline.TButton", command=browse_file_gptk)
 browse_file_gptk_btn.pack(pady=3)
-
-# runtime_label = ttk.Label(window, text="Runtime", font=font1)
-# runtime_label.place(x=11, y=119)
-# runtime_combobox = ttk.Combobox(window,values=runtimes, font=(font1, 14))
-# runtime_combobox.place(x=11,y=147)
 
 
 #bulid button
