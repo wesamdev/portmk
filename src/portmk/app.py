1ad4f177b1f3b096f2365b79ae5ec438084b31ad
wesam <software.market999@gmail.com>
2024-01-19 11:12:01 +0400

    v0.2-stable

--- src/portmk/app.py
+++ src/portmk/app.py
@@ -178,7 +178,7 @@ def browse_file_gptk():
 window = ttk.Window(themename="darkly")
 window.geometry("800x600")
 window.resizable(False, False)
-window.title("PORTMK  v0.2-dev")
+window.title("PORTMK  v0.2-stable")
 
 
 #Setup Fonts
@@ -231,7 +231,7 @@ bulid_button = ttk.Button(window, text="Bulid", bootstyle=SUCCESS, style="primai
 bulid_button.pack(pady=50)
 
 #version text
-version_label = ttk.Label(window, text="v0.2-dev", font=font1)
+version_label = ttk.Label(window, text="v0.2-stable", font=font1)
 version_label.pack(pady=6)
 
 if __name__ == "__main__":
